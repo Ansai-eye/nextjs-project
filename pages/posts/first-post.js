1df28793566309b8b1c5c176e8a263585ca79a89
Chuangshi Jiang <18251984729@163.com>
2021-09-27 15:27:15 +0800

    continue on next

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,17 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Head from "next/head";
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
-  return <>
-    <h1>Fist Post</h1>
-    <Link href="/">
-      <a>Back to home</a>
-    </Link>
-  </>;
+  return (
+    <Layout>
+      <Head>
+        <title>   First Post</title>
+      </Head>
+      <h1>Fist Post</h1>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </Layout>
+  );
 }
